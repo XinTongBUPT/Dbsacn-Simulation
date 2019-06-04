5a9d63ebdedee7dedac94046198e82cb38b00596
Xin Tong <xintongbupt@outlook.com>
2019-06-04 10:20:23 -0500

    Update

--- sketch.js
+++ sketch.js
@@ -6,16 +6,19 @@ var epsilon, epsilonSlider, epsilonSpan, minPoints, minPointsSlider, start, rese
 var data = [];
 var c = [];
 var isstart = true;
-var colors = ["#03A9F4", "#F8BBD0", "#FFEB3B", "#4CAF50", "#009688","#AA00FF", "#5C6BC0", "#BDBDBD", "#FFFF8D", "#795548","#F44336", "#9C27B0", "#E91E63", "#2196F3", "#8BC34A", "white"];
+var colors = ["rgb(3,169,244)", "rgb(248,187,208)","rgb(255,215,0)", "rgb(76,175,80)","rgb(170,0,255)","rgb(244,67,54)","rgb(0,0,255)","rgb(0,255,0)","rgb(255,0,0)","rgb(0,255,255)","rgb(128,128,360)","rgb(121,85,72)","rgb(65,105,225)","rgb(255,0,255)","rgb(255,255,255)" ];
 var colorc = 0;
+var newColor;
+
+
 
 
 function setup () {
   //creates a canvas for us to draw or display the output.
-  createCanvas(windowWidth, windowHeight - 60);
+  createCanvas(windowWidth-60, windowHeight - 60);
   createSpan("  |  ");
   createSpan("Epsilon : ");
-  epsilonSlider = createSlider(0, 400, 100);
+  epsilonSlider = createSlider(0, 300, 90);
   epsilonSlider.changed(epsilonChanged);
   epsilonSpan = createSpan(epsilonSlider.value());
   createSpan("  |  ");
@@ -29,15 +32,15 @@ function setup () {
     showline = !showline;
   });
   createSpan("  |  ");
-  showCircle = createButton("Show Circle");
+  showCircle = createButton("Show Fill");
   showCircle.mousePressed(function () {
     showcircle = !showcircle;
   });
-  // start = createButton("Start");
-  // start.mousePressed(startClustering);
+  start = createButton("Start");
+  start.mousePressed(startClustering);
 
-  // reset = createButton("Reset");
-  // reset.mousePressed(resetClustering);
+  clear = createButton("Clear");
+  clear.mousePressed(clearCluster);
 
 }
 
@@ -62,65 +65,82 @@ function draw () {
 
   noFill();
   noStroke();
+
+  for (var i = 0; i < data.length; i++) {
+    fill(color(colors.length-1));
+    ellipse(data[i][0], data[i][1], 10, 10);
+    noFill();
+    stroke('rgba(255,255,255,0.1)');
+    ellipse(data[i][0], data[i][1], epsilon, epsilon);
+  }
+
   for (var i = 0; i < data.length; i++) {
     noStroke();
+    newColor = colors[c[i]].substring(4,colors[c[i]].length-1);
+    // console.log(newColor);
     fill(color(colors[c[i]]));
     ellipse(data[i][0], data[i][1], 10, 10);
-    if (showcircle) {
-      noFill();
-      stroke(	176,196,222);
+    
+    if (showcircle && newColor!='255,255,255') {
+      // var millisecond = millis();
+      fill('rgba('+newColor+',0.15)');
+      // stroke('rgba('+newColor+',0.1)');
+      noStroke
       ellipse(data[i][0], data[i][1], epsilon, epsilon);
     }
   }
   if (isstart) {
-  for (var i = 0; i < data.length; i++) {
-    var temp = [];
-    var mintemp = 0;
-    for (var j = 0; j < data.length; j++) {
-      if (distanceSquare(data[i][0], data[i][1], data[j][0], data[j][1]) <= epsilon * epsilon) {
-        temp.push(j);
-        mintemp++;
-      }
-    }
-    if (mintemp >= minPoints) {
-
-      if (c[i] == colors.length - 1) {
-        c[i] = colorc;
-        colorc++;
-        colorc = colorc%colors.length;
-      }
-
-      for (var j = 0; j < temp.length; j++) {
-        //Sets the color used to draw lines and borders around shapes.
-        stroke(255);
-        c[temp[j]] = c[i];
-        if (showline) {
-          line(data[i][0], data[i][1], data[temp[j]][0], data[temp[j]][1]);
+    for (var i = 0; i < data.length; i++) {
+        var temp = [];
+        var mintemp = 0;
+        for (var j = 0; j < data.length; j++) {
+          if (distanceSquare(data[i][0], data[i][1], data[j][0], data[j][1]) <= epsilon * epsilon) {
+            temp.push(j);
+            mintemp++;
+          }
         }
-        noStroke();
-      }
+        if (mintemp >= minPoints) {
+
+          if (c[i] == colors.length - 1) {
+            c[i] = colorc;
+            colorc++;
+            colorc = colorc%colors.length;
+          }
+
+          for (var j = 0; j < temp.length; j++) {
+            //Sets the color used to draw lines and borders around shapes.
+            stroke(	'rgba(255,255,255,0.3)');
+            c[temp[j]] = c[i];
+            if (showline) {
+              line(data[i][0], data[i][1], data[temp[j]][0], data[temp[j]][1]);
+            }
+            noStroke();
+          }
+        }
     }
-  }
   }
   noFill();
 }
 
-// function startClustering () {
-//   isstart = !isstart;
-
-//   epsilonSlider.hide();
-//   minPointsSlider.hide();
-// }
-
-function resetClustering () {
-
-  for (var i = 0; i < data.length; i++) {
-    c[i] = colors.length - 1;
+function startClustering () {
+  data = [];
+  c = [];
+  // isstart = !isstart;
+  // epsilonSlider.hide();
+  // minPointsSlider.hide();
+  for(var i = 0; i < 100; ++i){
+    var temp = [Math.random()*(windowWidth-20)+50,Math.random()*(windowHeight - 150)+50];
+    data[i]=temp;
+    c.push(colors.length - 1);
   }
-  isstart = !isstart;
+  draw ();
+}
 
-  epsilonSlider.show();
-  minPointsSlider.show();
+function clearCluster () {
+  data = [];
+  c = [];
+  // epsilonSlider.show();
+  // minPointsSlider.show();
 }
 
 function mouseClicked () {
